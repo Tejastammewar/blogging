1171e45bf43e792031720eeb51f2122a8dafd9ee
Tejastammewar <tejastammewar@gmail.com>
2023-11-23 11:43:31 +0530

    path

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import ".//Create.css";
+import "./Create.css";
 function Create() {
   const [title, setTitle] = useState();
   const [content, setContent] = useState();
